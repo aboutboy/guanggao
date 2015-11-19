b2a74fe5b8ac940d3d71afdf694ecf6e43af8748
root <root@localhost.localdomain>
2015-11-18 23:10:10 -0500

    android make

---
+++ Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE:= webad
+
+LOCAL_SRC_FILES := \
+    main.c \
+    libipq.c \
+    mpool.c \
+    msocket.c \
+    plug.c \
+    queue.c \
+    thpool.c \
+    util.c
+
+LOCAL_LDLIBS = -lpthread
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_SHARED_LIBRARIES := libc
+
+include $(BUILD_EXECUTABLE)
